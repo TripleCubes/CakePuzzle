872abeb1a35eecd4631a59a0ce26a26cee6e4631
TripleCubes <113813506+TripleCubes@users.noreply.github.com>
2023-12-24 17:13:20 +0700

    Add 3d effects to pieces

---
+++ CakePuzzle/Scripts/Piece/piece_height.gd
@@ -0,0 +1,15 @@
+extends ColorRect
+
+@onready var _sprite = get_parent().get_node("Sprite2D")
+
+func _ready():
+	material = material.duplicate()
+	material.set_shader_parameter("sprite_size", size)
+	material.set_shader_parameter("piece_size", _sprite.piece_size)
+	material.set_shader_parameter("offset", _sprite.piece_offset)
+	material.set_shader_parameter("padding", Vector2(20, 20))
+	material.set_shader_parameter("up_type", _sprite.up_type)
+	material.set_shader_parameter("down_type", _sprite.down_type)
+	material.set_shader_parameter("left_type", _sprite.left_type)
+	material.set_shader_parameter("right_type", _sprite.right_type)
+	material.set_shader_parameter("height_draw", true)
--- CakePuzzle/Scripts/Piece/piece_sprite.gd
+++ CakePuzzle/Scripts/Piece/piece_sprite.gd
@@ -17,3 +17,4 @@ func _ready():
 	material.set_shader_parameter("down_type", down_type)
 	material.set_shader_parameter("left_type", left_type)
 	material.set_shader_parameter("right_type", right_type)
+	material.set_shader_parameter("height_draw", false)
